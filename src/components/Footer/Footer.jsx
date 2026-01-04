1c88f0bd420cc126055db8e10c98945a1ddd4650
Colin Huber <cbhuber@gmail.com>
2026-01-03 21:35:41 -0700

    feat: Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+export default function Footer() {
+  const lastUpdated = "January 3, 2026";
+
+  return (
+    <footer className="site-footer">
+      <p className="site-footer__text">
+        Page last updated at:{" "}
+        <span className="site-footer__date">{lastUpdated}</span>
+      </p>
+    </footer>
+  );
+}
