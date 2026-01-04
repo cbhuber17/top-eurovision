90f32284598da091e8c9bde44ae917a67ec13b7f
Colin Huber <cbhuber@gmail.com>
2026-01-03 19:09:43 -0700

    feat: Page header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="site-header">
+      <h1 className="site-title">Huber Eurovision Top 10</h1>
+    </header>
+  );
+}
